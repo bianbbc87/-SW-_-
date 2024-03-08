f1e1a569989a58842ecc10f81c554523f32627e3
eunji <bian87@dgu.ac.kr>
2024-03-08 14:05:09 +0900

    정은지 2021112828

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # 입력받기
 name = input("이름을 입력하세요: ")
 student_id = input("학번을 입력하세요: ")
+department= input("학과를 입력하세요: ")
 
 # 출력하기
 print("입력된 이름:", name)
 print("입력된 학번:", student_id)
+print("입력된 학과: ", department)
